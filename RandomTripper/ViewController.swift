afae8689fb74c6d03e5dee75e5a059c8170d512d
mamo1016 <rr0061vv@ed.ritsumei.ac.jp>
2019-05-14 17:32:54 +0900

    Initial Commit

---
+++ RandomTripper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RandomTripper
+//
+//  Created by 松尾大雅 on 2019/05/10.
+//  Copyright © 2019 litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func backfood(segue: UIStoryboardSegue) {}
+    @IBAction func backstation(segue: UIStoryboardSegue) {}
+    @IBAction func backsave(segue: UIStoryboardSegue) {}
+
+    
+    
+    
+    
+    
+}
+
